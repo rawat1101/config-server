6bec63f442821dc6dfc6371d61d5930ae02d07e2
Mahendra Rawat <mahendra.rawat@gaana.com>
2021-06-07 12:11:06 +0530

    up

--- configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
@@ -1,7 +1,6 @@
 
 package com.example.configurationservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
