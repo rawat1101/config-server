bf2ee1c8c3944a269765236db716a1082e3bb421
Mahendra Rawat <mahendra.rawat@gaana.com>
2021-06-11 19:41:55 +0530

    up

--- configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/example/configurationservice/ConfigurationServiceApplication.java
@@ -5,27 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.core.env.Environment;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServiceApplication implements CommandLineRunner {
 	/*@Value("${ENCRYPT_KEY}")
 	private String key;*/
+	@Autowired
+	Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigurationServiceApplication.class, args);
-		System.out.println(
-				"======================== " + System.getenv("JAVA_HOME") + " ==============================");
-//				"======================== " + System.getenv("ENCRYPT_KEY") + " ==============================");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-//		System.out.println("======================== " + key + " ==============================");
+		System.out.println("================= " + System.getenv("JAVA_HOME") + " ==========");
+		System.out.println("================= " + System.getenv("ENCRYPT_KEY") + " ==========");
+		System.out.println("========= " + System.getenv("encrypt.key") + " ==================");
+		System.out.println("========= " + env.getProperty("ENCRYPT_KEY") + " ==================");
+		System.out.println("========= " + env.getProperty("encrypt.key") + " ==================");
 	}
 }
